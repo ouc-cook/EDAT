c0e095045e79361e6bba0f760a345e2094155488
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-07-27 14:06:31 +0200

    void

--- P02_analyzedTracks2maps.m
+++ P02_analyzedTracks2maps.m
@@ -1,8 +1,7 @@
-function P02_analyzedTracks2maps
-    
+function P02_analyzedTracks2maps    
     DD = initialise;
     window = getfieldload(DD.path.windowFile,'window');
-    main(DD,window);
+    main(DD,window);    
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function main(DD,window)
@@ -10,17 +9,14 @@ function main(DD,window)
     [FN,tracks,txtFileName] = initTxtFileWrite(DD);
     %%
     writeToTxtFiles(txtFileName,FN,tracks);
-    %  TODO
-    %         buildNetCdfFromTxtFiles(FN,txtFileName)
-    %
+    %%
     meanMap =  initMeanMaps(window);
-    %
-    meanMap = buildMeanMaps(meanMap,FN,txtFileName,DD.threads.num); %#ok<NASGU>
+    %%
+    meanMap = buildMeanMaps(meanMap,txtFileName,DD.threads.num);
     %%
     meanMap.birthDeath = buildBirthDeathMaps(tracks);
     %%
     save([DD.path.root,'meanMaps.mat'],'meanMap');
-    a = load([DD.path.root,'meanMaps.mat']);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function BD = buildBirthDeathMaps(tracks)
@@ -39,7 +35,7 @@ function BD = buildBirthDeathMaps(tracks)
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % init output map dim
-function map = initMeanMaps(window)
+function map = initMeanMaps(window) % TODO make better
     geo = window.geo;
     %     bs  = DD.map.out.binSize;
     %%
@@ -53,95 +49,33 @@ function map = initMeanMaps(window)
     [map.lon,map.lat] = meshgrid(xvec,yvec);
 end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function buildNetCdfFromTxtFiles(FN,txtFileName)
-    return
-    % TODO
-    for ii=1:numel(FN); fn = FN{ii};
-        f.(fn) = fopen(txtFileName.(fn), 'r');
-    end
-    lat=fscanf(f.lat,'%f');
-    lon=fscanf(f.lon,'%f');
-    u=fscanf(f.u,'%f');
-    v=fscanf(f.v,'%f');
-    N=numel(a);
-    nccreate('bla.nc','lat',...
-        'Dimensions',{'x',1,'N',N},...
-        'Format','classic')
-    
-    nccreate('bla.nc','lon','Dimensions',{'x',1,'N',N})
-    nccreate('bla.nc','u','Dimensions',{'x',1,'N',N})
-    nccreate('bla.nc','v','Dimensions',{'x',1,'N',N})
-    
-    ncwrite('bla.nc','lat',lat')
-    ncwrite('bla.nc','lon',lon')
-    ncwrite('bla.nc','u',u')
-    ncwrite('bla.nc','v',v')
-    
-end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-function meanMaps = buildMeanMaps(meanMaps,FN,txtFileName,threads)
+function meanMaps = buildMeanMaps(meanMaps,txtFileName,threads)
     %% init
     [Y,X] = size(meanMaps.lat);
-    meanMaps.count = zeros(Y,X);
-    meanMaps.u     = zeros(Y,X);
-    meanMaps.v     = zeros(Y,X);
-    meanMaps.scale = zeros(Y,X);
+    
     %% read lat lon vectors
     lat = fscanf(fopen(txtFileName.lat, 'r'), '%f ');
     lon = wrapTo360(fscanf(fopen(txtFileName.lon, 'r'), '%f '));
+    
     %% find index in output geometry
-        
-    lalo = lon + 1i*lat;
-    Mlalo = reshape(meanMaps.lon + 1i*meanMaps.lat,[],1);
-    idxlin = nan(size(lalo));
-    
-    lims = thread_distro(threads,numel(lalo));
-    spmd(threads)
-        T = disp_progress('init','blibb');
-        for ii=lims(labindex,1):lims(labindex,2)
-            T = disp_progress('blubb',T,diff(lims(labindex,:)),1000);
-            [minA,minAidx] = min(abs(lalo(ii)-Mlalo)) ;
-            [minB,minBidx] = min(abs(lalo(ii)-Mlalo+360)) ;
-            if minA < minB
-                idxlin(ii) = minAidx;
-            else
-                idxlin(ii) = minBidx;
-            end
-        end
-        idxx = gop(@horzcat,idxlin,1);
-    end
-    idxx = idxx{1};
-    idxlin = nansum(idxx,2);
-    
+    idxlin = binDownGlobalMap(lat,lon,meanMaps.lat,meanMaps.lon);
     
     %% read parameters
     u     = fscanf(fopen(txtFileName.u, 'r'),     '%e ');
     v     = fscanf(fopen(txtFileName.v, 'r'),     '%e ');
     scale = fscanf(fopen(txtFileName.scale, 'r'), '%e ');
-    %% sum over parameters for each grid cell
-    % TODO make faster
     
-    for kk = 1:X*Y
-        fprintf('%2.1f%%\n',round(1000*kk/X/Y)/10)
-        flag = (idxlin == kk);
-        meanMaps.count(kk) = meanMaps.count(kk) + sum(flag);
-        meanMaps.u(kk) = meanMaps.u(kk) + sum(u(flag));
-        meanMaps.v(kk) = meanMaps.v(kk) + sum(v(flag));
-        meanMaps.scale(kk) = meanMaps.scale(kk) + sum(scale(flag));
-    end
-    %% build means
-    meanMaps.count(meanMaps.count==0)   = nan;
-    meanMaps.u     = meanMaps.u     ./ meanMaps.count;
-    meanMaps.v     = meanMaps.v     ./ meanMaps.count;
-    meanMaps.scale = meanMaps.scale ./ meanMaps.count;
+    %% sum over parameters for each grid cell
+    meanMaps.u = meanMapOverIndexedBins(u,idxlin,Y,X,threads);
+    meanMaps.v = meanMapOverIndexedBins(v,idxlin,Y,X,threads);
+    meanMaps.scale = meanMapOverIndexedBins(scale,idxlin,Y,X,threads);
+    
     %% calc angle
     uv               = meanMaps.u + 1i * meanMaps.v;
     meanMaps.absUV   = abs(uv) ;
     meanMaps.angleUV = reshape(wrapTo360(rad2deg(phase(uv(:)))),Y,X);
     
 end
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 function [FN,tracks,txtFileName] = initTxtFileWrite(DD)
     tracks = DD.path.tracks.files;
@@ -178,6 +112,32 @@ end
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
-
-
-
+% 
+% 
+% 
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% function buildNetCdfFromTxtFiles(FN,txtFileName)
+%     return
+%     % TODO
+%     for ii=1:numel(FN); fn = FN{ii};
+%         f.(fn) = fopen(txtFileName.(fn), 'r');
+%     end
+%     lat=fscanf(f.lat,'%f');
+%     lon=fscanf(f.lon,'%f');
+%     u=fscanf(f.u,'%f');
+%     v=fscanf(f.v,'%f');
+%     N=numel(a);
+%     nccreate('bla.nc','lat',...
+%         'Dimensions',{'x',1,'N',N},...
+%         'Format','classic')
+%     
+%     nccreate('bla.nc','lon','Dimensions',{'x',1,'N',N})
+%     nccreate('bla.nc','u','Dimensions',{'x',1,'N',N})
+%     nccreate('bla.nc','v','Dimensions',{'x',1,'N',N})
+%     
+%     ncwrite('bla.nc','lat',lat')
+%     ncwrite('bla.nc','lon',lon')
+%     ncwrite('bla.nc','u',u')
+%     ncwrite('bla.nc','v',v')
+%     
+% end
