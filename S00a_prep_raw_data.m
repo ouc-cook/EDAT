9be44fb779686bc7fc339b089a9c1d7d7621bd2e
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-08-03 12:44:39 +0200

    void

--- S00a_prep_raw_data.m
+++ S00a_prep_raw_data.m
@@ -1,18 +1,19 @@
 % This is the first 'pre'-step (S00..). It's purpose is to extract SSH data
-% from either pop or aviso files named like eg (15/07)
-% SSH_GLB_t.t0.1_42l_CORE.yyyymmdd.nc or
+% from either pop or aviso files named like eg 
+% SSH_GLB_t.t0.1_42l_CORE.yyyymmdd.nc 
+% or
 % dt_global_twosat_msla_h_yyyymmdd_20140106.nc.
 %
 % the function
 % -first creates the geometric information needed to transform
-% input SSH to desired output geometry
+% input SSH to desired output geometry (see window3.m)
 % -and then saves one output .mat SSH file per time-step to
 % ../dataXXX/CUTS/
 %
 % geo information is stored in ../dataXXX/window.mat.
 %
-% All S00 steps are not officially part of the program as they have to be
-% rewritten/adapted to produce the needed format for S01.. etc.
+% remember to run addpath(genpath('./')) when starting matlab
+
 %% init dependencies
 addpath(genpath('./'));
 %% set up meta-data/info file "DD.mat" and get user input
