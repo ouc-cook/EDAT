f99feff288a56e95317241e7393af5bf0520bbeb
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-08-11 12:29:07 +0200

    void

--- P03_plotting.m
+++ P03_plotting.m
@@ -2,9 +2,9 @@ DD = initialise;
 window = getfieldload(DD.path.windowFile,'window');
 meanMaps = getfieldload(sprintf('%smeanMaps.mat',DD.path.root),'meanMap');
 %%
-% subP03_meanMaps(DD,window,meanMaps);
+subP03_meanMaps(DD,window,meanMaps);
 %%
-subP03_makeNetCdf(DD,window,meanMaps);
+% subP03_makeNetCdf(DD,window,meanMaps);
 
 
 
--- SUBS/subP03_makeNetCdf.m
+++ SUBS/subP03_makeNetCdf.m
@@ -1,20 +1,26 @@
 function subP03_makeNetCdf(DD,window,meanMaps)
-
+    
+    [Y,X] = size(meanMaps.lon);
+    ncF   = [DD.path.root 'binnedMeanMaps.nc'];
+    
+    nccreate(ncF,'u','Dimensions',{'x',X,'y',Y});
+    nccreate(ncF,'v','Dimensions',{'x',X,'y',Y});
+    nccreate(ncF,'long','Dimensions',{'x',X,'y',Y});
+    nccreate(ncF,'lat','Dimensions',{'x',X,'y',Y});
+    nccreate(ncF,'radius','Dimensions',{'x',X,'y',Y});
+    nccreate(ncF,'absU','Dimensions',{'x',X,'y',Y});
+    nccreate(ncF,'angleU','Dimensions',{'x',X,'y',Y});
+    
+    ncwrite(ncF,'u',permute(meanMaps.u,[2 1]));
+    ncwrite(ncF,'v',permute(meanMaps.v,[2 1]));
+    ncwrite(ncF,'long',permute(meanMaps.lon,[2 1]));
+    ncwrite(ncF,'lat',permute(meanMaps.lat,[2 1]));
+    ncwrite(ncF,'radius',permute(meanMaps.scale,[2 1]));
+    ncwrite(ncF,'absU',permute(meanMaps.absUV,[2 1]));
+    ncwrite(ncF,'angleU',permute(meanMaps.angleUV,[2 1]));
     
     
     
-    ncF='geostrVelsFromPop.nc';
-system(sprintf('rm %s',ncF));
-nccreate(ncF,'Ug','Dimensions',{'x',X,'y',Y,'z',Z});
-nccreate(ncF,'Vg','Dimensions',{'x',X,'y',Y,'z',Z});
-nccreate(ncF,'lat','Dimensions',{'x',X,'y',Y});
-nccreate(ncF,'lon','Dimensions',{'x',X,'y',Y});
-nccreate(ncF,'depth','Dimensions',{'z',Z});
-ncwrite(ncF,'Ug',permute(Ug,[3,2,1]))
-ncwrite(ncF,'Vg',permute(Vg,[3,2,1]))
-ncwrite(ncF,'lat',permute(lat,[2,1]))
-ncwrite(ncF,'lon',permute(lon,[2,1]))
-ncwrite(ncF,'depth',depth.inside)
-
-system(sprintf('ncview %s',ncF))
+    
+    system(sprintf('ncview %s',ncF))
 end
