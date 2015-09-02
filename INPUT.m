5b9c55c1009a309820fb04eb105d90cbebc01671
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-09-02 15:12:22 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -3,26 +3,27 @@
 % 'aviso' - template for AVISO SSH data
 function DD=INPUT
     %DD.template = 'aviso';
-    DD.template = 'pop';
+%     DD.template = 'pop';
 %     DD.template = 'pop2avi';
+    DD.template = 'depth';
     
     %% threads / debug
-    DD.threads.num = 4;
+    DD.threads.num = 12;
     
     %% overwrite data
     DD.overwrite = false;
     
     %% time
-    DD.time.from.str  = '19990101'; %first pop/avi
-    DD.time.till.str  = '19991001';
+    DD.time.from.str  = '19940201'; %first pop/avi
+    DD.time.till.str  = '20000101';
     DD.time.delta_t   = 7; % [days]!
-    threshlife        = 35;
+    threshlife        = 21;
     
     %% window on globe (0:360° system)
     DD.map.in.west  =  0;
-    DD.map.in.east  =  30;
-    DD.map.in.south = -50;
-    DD.map.in.north = -30;
+    DD.map.in.east  =  360;
+    DD.map.in.south = -70;
+    DD.map.in.north =  0;
     
     %% thresholds
     DD.contour.step                = 0.01; % [SI]
