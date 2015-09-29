ad5e98ced1726f455ea155b566cf5e97502bcd5a
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-09-29 14:27:42 +0200

    void

--- INPUT.m
+++ INPUT.m
@@ -2,30 +2,27 @@
 % 'pop' - template for POP SSH data
 % 'aviso' - template for AVISO SSH data
 function DD=INPUT
-    DD.template = 'aviso';
-%     DD.template = 'pop';
-    %     DD.template = 'pop2avi';
-    %  DD.template = 'depth';
-%      DD.template = 'bla';
-
+    %DD.template = 'aviso';
+    DD.template = 'pop';
+    
     %% threads / debug
-    DD.threads.num = 12;
-
+    DD.threads.num = 1;
+    
     %% overwrite data
     DD.overwrite = false;
-
+    
     %% time
-    DD.time.from.str  = '19990101'; %first pop/avi
-    DD.time.till.str  = '19991001';
-    DD.time.delta_t   = 14; % [days]!
-    threshlife        = 14*3;
-
+    DD.time.from.str  = '19940105'; %first pop/avi
+    DD.time.till.str  = '20061226';
+    DD.time.delta_t   = 2; % [days]!
+    threshlife        = 20;
+    
     %% window on globe (0:360° system)
-    DD.map.in.west  =  90;
-    DD.map.in.east  =  120;
-    DD.map.in.south = -50;
-    DD.map.in.north = -30;
-
+    DD.map.in.west  =  0;
+    DD.map.in.east  =  360;
+    DD.map.in.south = -80;
+    DD.map.in.north =  80;
+    
     %% thresholds
     DD.contour.step                = 0.01; % [SI]
     DD.thresh.maxRadiusOverRossbyL = 4; %[ ]
@@ -36,13 +33,13 @@ function DD=INPUT
     DD.thresh.corners.max          = 500; % dont make too small! [ ]
     DD.thresh.life                 = threshlife; % min num of living days for saving [days]
     DD.thresh.IdentityCheck        = 2; % 1: perfect fit, 2: 100% change ie factor 2 in either sigma or amp
-%     DD.thresh.phase                = 42; % max(abs(rossby phase speed)) (upper lim to c-field) [SI]
-
+    DD.thresh.phase                = 42; % max(abs(rossby phase speed)) (upper lim to c-field) [SI]
+    
     %% parameters
     DD.parameters.fourierOrder	   = 4;
     DD.parameters.zoomIncreaseFac  = 6;
     DD.parameters.minProjecDist    = 350e3; % (per week)  minimum linear_eccentricity*2 of ellipse (see chelton 2011)
     DD.parameters.trackingRef      = 'CenterOfVolume'; % choices: 'centroid', 'CenterOfVolume', 'Peak'
     DD.parameters.rossbySpeedFactor= 1.75; % eddy translation speed assumed factor*rossbyWavePhaseSpeed for tracking projections
-
+    
 end
