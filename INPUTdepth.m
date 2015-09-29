46b1941cc8aa63fdea3fa97d97fb9f84040a62e9
Nikolaus Koopmann <nikolaus.koopmann@zmaw.de>
2015-09-29 14:06:15 +0200

    auto commit before refresh

--- INPUTdepth.m
+++ INPUTdepth.m
@@ -1,5 +1,6 @@
 function DD=INPUTdepth
     
+    %     zz = 22;
     zz = 22;
     
     %% dirs
@@ -9,13 +10,13 @@ function DD=INPUTdepth
     %% map in keys
     DD.map.in.fname    = ['PSEUDOSSH_GLB_t.t0.1_42l_CORE.yyyymmdd_at-z' num2str(zz) '.nc'];
     DD.map.in.keys.lat = 'U_LAT_2D';
-    DD.map.in.keys.lon = 'U_LON_2D';   
+    DD.map.in.keys.lon = 'U_LON_2D';
     DD.map.in.keys.ssh = 'SSH';
-       
+    
     %% parameters
     DD.parameters.ssh_unitFactor = 100; % eg 100 if SSH data in cm, 1/10 if in deka m etc..
     
-     %% switches
+    %% switches
     DD.switches.isAnomaly = false;
-        
+    
 end
